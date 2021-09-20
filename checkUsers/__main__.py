3188b3750b7dfc94b13547baaf22f6697d270d72
nickyBlackBurn <nickblackburn02@gmail.com>
2021-09-20 09:15:47 -0400

    added windows and linux platfom detection

--- checkUsers/__main__.py
+++ checkUsers/__main__.py
@@ -1,3 +1,42 @@
 """
 this class is where allthe code is executed when ran 
 """
+
+
+import imports 
+import ColorLog
+
+
+def Main():
+
+    
+
+    ColorLog.Warning("Starting Up Check Users...")
+
+    
+    # Runs on Windows platform
+    if(imports.platform.system()== 'Windows'):  
+        username = imports.os.getlogin()
+
+        ColorLog.PipeLine_Ok('Running on windows... as the user'+ ' '+str(username))
+        
+
+       # Runs on Linux platform
+    if(imports.platform.system()== 'Linux'):  
+        username = imports.os.getlogin()
+
+        ColorLog.PipeLine_Ok('Running on Linux... as the user'+ ' '+str(username))
+        
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    Main()
--- checkUsers/imports.py
+++ checkUsers/imports.py
@@ -18,3 +18,5 @@ from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import getpass
+import os 
