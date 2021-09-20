a7bb05e5546bec5321f5471c93ff0a90d0d6a8ff
nickyBlackBurn <nickblackburn02@gmail.com>
2021-09-20 08:59:44 -0400

    added Color log and imports

--- checkUsers/imports.py
+++ checkUsers/imports.py
@@ -1,3 +1,20 @@
 """
 this file is for having module wide imports so i can make project easier to manage for my self 
 """
+import logging
+from datetime import datetime
+import smtplib, ssl
+
+import configparser
+import pathlib
+import platform
+import sys
+from io import StringIO
+
+from colorama import Fore, Back, Style
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
