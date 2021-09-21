a48e76718de35720eb8cc5b69da732bcc54943e0
nickyBlackBurn <nickblackburn02@gmail.com>
2021-09-21 11:58:44 -0400

    added etc

--- syncro.ps1
+++ syncro.ps1
@@ -1,22 +1,18 @@
-
-/**
- *
- * # this script downloades srip runnings and runs the python scrip on the target pc 
- */
-
+ # this script downloades srip runnings and runs the python scrip on the target pc 
 $git_url = "https://api.github.com/repos/git-for-windows/git/releases/latest"
 $asset = Invoke-RestMethod -Method Get -Uri $git_url | % assets | where name -like "*64-bit.exe"
 # download installer
 $installer = "$env:temp\$($asset.name)"
 Invoke-WebRequest -Uri $asset.browser_download_url -OutFile $installer
 # run installer
-$git_install_inf = "<install inf file>"
-$install_args = "/SP- /VERYSILENT /SUPPRESSMSGBOXES /NOCANCEL /NORESTART /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /LOADINF=""$git_install_inf"""
+$install_args = "/SP- /VERYSILENT /SUPPRESSMSGBOXES /NOCANCEL /NORESTART /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS"
 Start-Process -FilePath $installer -ArgumentList $install_args -Wait
 
 
+Start-Sleep -s 10
+
+refreshevn
+
 git clone https://github.com/NicholasBlackburn1/Checky-users.git
 
-cd .\Checky-users
-
-powershell -noexit .\run.ps1
+powershell -noexit .\Checky-users\run.ps1
